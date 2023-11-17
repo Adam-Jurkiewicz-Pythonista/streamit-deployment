862d53d4dd3e89e7bd6ecbb759b3da0e94f39d9f
Adam Jurkiewicz <adam@jurkiewicz.tech>
2023-11-17 10:32:28 +0100

    zmiany

--- application_pkw/app_pkw.py
+++ application_pkw/app_pkw.py
@@ -1,6 +1,9 @@
 import streamlit as st
 import pandas as pd
 
-dane_pkw = pd.read_excel("datas/wyniki_gl_na_listy_po_obwodach_proc_sejm_utf8.xlsx")
-
-edited_df = st.data_editor(dane_pkw, num_rows="dynamic")
+try:
+    dane_pkw = pd.read_excel("wyniki.xlsx")
+    edited_df = st.data_editor(dane_pkw, num_rows="dynamic")
+except Exception as e:
+    st.header('Błąd wczytywania')
+    st.subheader(f"{e=}")

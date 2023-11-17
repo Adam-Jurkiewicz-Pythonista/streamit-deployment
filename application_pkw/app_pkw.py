be4a75c30af19951d0b6dad6f27918d6a91acf64
Adam Jurkiewicz <adam@jurkiewicz.tech>
2023-11-17 11:07:23 +0100

    zmiany

--- application_pkw/app_pkw.py
+++ application_pkw/app_pkw.py
@@ -2,11 +2,6 @@ import streamlit as st
 
 from streamlit_gsheets import GSheetsConnection
 
-def reload_datas():
-    conn = st.connection("gsheets", type=GSheetsConnection)
-    dane_pkw = conn.read()
-    edited_df = st.data_editor(dane_pkw, num_rows="dynamic")
-
 try:
     # Create a connection object.
     conn = st.connection("gsheets", type=GSheetsConnection)
@@ -16,5 +11,13 @@ try:
 except Exception as e:
     st.header('Błąd wczytywania')
     st.subheader(f"{e=}")
+
+def reload_datas():
+    try:
+        dane_pkw = conn.read()
+        st.data_editor(dane_pkw, num_rows="dynamic")
+    except Exception as e:
+        st.header('Błąd reload')
+        st.subheader(f"{e=}")
 
 st.button("RELOAD", on_click="reload_datas")
